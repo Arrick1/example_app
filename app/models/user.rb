bb41715967d181127a631cf716904b43b4376f17
Arrick D <arrickdunlap@gmail.com>
2021-12-02 08:43:26 +0000

    email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,6 @@
-class User
-  attr_accessor :name, :email
-  
-  def initialize(attributes ={})
-    @name = attributes[:name]
-    @email = attributes [:email]
-  end
-  
-  def formatted_email
-    "#{@home} <#{@email}"
-  end
-
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence:   true, length: { maximum: 255 },
+                    format:     { with: VALID_EMAIL_REGEX }
 end
