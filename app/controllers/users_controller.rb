b01e80104fe6a3aceefac8bb33839c76cb0a284d
Arrick D <arrickdunlap@gmail.com>
2022-01-19 20:41:32 +0000

    unsuccessful edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,13 @@
 class UsersController < ApplicationController
- 
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -19,7 +19,20 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def user_params
