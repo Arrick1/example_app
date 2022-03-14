63fd4849ecaaa16e4050fc909e71e37d2a04a0cd
Arrick D <arrickdunlap@gmail.com>
2022-03-14 23:35:48 +0000

    Add account activations

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,9 @@ module SessionsHelper
   # Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
+    # Guard against session replay attacks.
+    # See https://bit.ly/33UvK0w for more.
+    session[:session_token] = user.session_token
   end
 
   # Remembers a user in a persistent session.
@@ -21,16 +24,16 @@ module SessionsHelper
       end
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
+      if user && user.authenticated?(:remember, cookies[:remember_token])
         log_in user
         @current_user = user
       end
     end
   end
-  
-   # Returns true if the given user is the current user.
+
+  # Returns true if the given user is the current user.
   def current_user?(user)
-    user && user == current_user
+    user == current_user
   end
 
   # Returns true if the user is logged in, false otherwise.
@@ -38,7 +41,7 @@ module SessionsHelper
     !current_user.nil?
   end
 
- # Forgets a persistent session.
+  # Forgets a persistent session.
   def forget(user)
     user.forget
     cookies.delete(:user_id)
@@ -51,8 +54,8 @@ module SessionsHelper
     reset_session
     @current_user = nil
   end
-  
-    # Stores the URL trying to be accessed.
+
+  # Stores the URL trying to be accessed.
   def store_location
     session[:forwarding_url] = request.original_url if request.get?
   end
